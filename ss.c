d9229d4b6b6358864d9e125c36644bf44f579f8c
sathvikabhogi <96292076+sathvikabhogi@users.noreply.github.com>
2021-12-18 15:18:04 +0530

    Add files via upload

---
+++ ss.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+   int num1,num2,diff;
+    printf("Enter  two number");
+    scanf("%d%d",&num1,  &num2);
+   
+    
+    diff=num1-num2;
+    printf("diff between %d and %d is %d",num1,num2,diff);
+}
