d29bb1f994e88c81b72cc29ec4014c0312da3448
sathvikabhogi <96292076+sathvikabhogi@users.noreply.github.com>
2021-12-18 15:23:56 +0530

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main ()
+{
+float radius,area,perimeter;
+    scanf("%f",&radius);
+    area=3.14*radius*radius;
+    perimeter=2*3.14*radius;
+    printf("area of a circle with %f is %f",radius);
+    printf("\nperimeter of a circle with %f",perimeter);
+}
+    
