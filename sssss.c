f26aaf8896045de3a921ed2b9e3267db0b0ed521
sathvikabhogi <96292076+sathvikabhogi@users.noreply.github.com>
2021-12-18 15:21:42 +0530

    Add files via upload

---
+++ sssss.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int side,area,perimeter;
+scanf("%d", &side);
+area=side*side;
+perimeter=4*side;
+printf("area of a square %d is %d",side ,area);
+printf("\nperimeter of a given square %d is %d" ,side,perimeter);
+}
